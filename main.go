f9101aefac1fb0d85c99f36fbd4f1f8de6203bd8
jonti09 <jontisavani@gmail.com>
2021-01-10 13:54:41 +0530

    FIX: flag checking was causing index out of bouds

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"github.com/jonti09/themer/server"
 )
 
+func hasDevFlag() bool {
+	return len(os.Args) == 2 && os.Args[1] == "--dev"
+}
+
 func main() {
-	if os.Args[1] != "--dev" {
+	if !hasDevFlag() {
 		server.Init()
 	}
 	server.StartAndListen()
